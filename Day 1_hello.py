eb52a47a151347a0b75d6a0ce836fd2f15f8e650
IndhuThuppathi <thuppathiindhu8@gmail.com>
2026-08-06 09:15:16 +0530

    Added Day 1 Python basics and tasks

---
+++ Day 1_hello.py
@@ -0,0 +1,86 @@
+
+#print()
+print("hello world")
+
+#variable
+
+#1.string
+first_name="Indhu"
+print(f" hello {first_name}")
+#2.integer
+age = 15
+print(f"you are {age} years old")
+discount=20
+print(f"you got rs {discount} ")
+#3.float
+price=10.99
+print(f"price of a product is ${price}")
+#4.boolean
+is_student=True
+print(f"are you a student?{is_student}")
+
+
+#typecasting
+
+name="indhu"
+print(type(name))
+
+gpa=3.2
+gpa=int(gpa)
+print(gpa)
+
+age=25
+age=float(age)
+print(age)
+age=25
+age=str(age)
+print(age)
+print(type(age))
+age+='1'
+
+print(age)
+
+name="b"
+name=bool(name)
+print(name)
+
+name=""
+name=bool(name)
+print(name)
+
+#use input
+
+name=input("what is your name?")
+age=input("what is your age?")
+
+age=23
+age=int(age)
+age+=1
+
+
+print(f"my name is {name}")
+print("happy birthday")
+print(f"my age is {age}")
+
+#ex-1->area of rectangle
+l=float(input("enter the length"))
+w=float(input("enter the width"))
+a=l*w
+print(f"the area of rectangle is{a}")
+
+#madlibs game
+#creating a story
+
+animal=input("enter animal")
+adj1=input("how it is")
+verb=input("what it is doing")
+noun=input("enter suitable noun")
+
+
+print(f"i went to {animal} park")
+print(f"i saw {adj1} monkey")
+print(f"it is {verb} ")
+print(f" {noun} was happy")
+
+
+
---
+++ Day1_tasks.py
@@ -0,0 +1,30 @@
+
+#Program 1: Personal Information
+name=input("enter you name")
+age=int(input("enter age"))
+branch=input("enter branch")
+college=input("enter your college")
+print(f"your name is {name}")
+print(f"your age is {age}")
+print(f"your branch is {branch}")
+print(f"your college is {college}")
+
+#Program 2: Age Calculator
+age=int(input("enter you current age indhu"))
+
+age+=10
+
+print(f"after 10 years your age will be: {age}")
+
+#Program 3: Simple Calculator
+
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+c1=a+b
+c2=a-b
+c3=a*b
+c4=a/b
+print(f"addition:{c1}")
+print(f"substaction:{c2}")
+print(f"multiplication:{c3}")
+print(f"division:{c4}")
